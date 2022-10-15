45e54388cf9a267baea9179477bc215662975281
henry-2112 <112739782+henry-2112@users.noreply.github.com>
2022-10-15 15:39:10 -0400

    Create sodaTester_ExtraCredit.java

---
+++ sodaTester_ExtraCredit.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class sodaTester_ExtraCredit here.
+ *
+ * @author (Henry St. Pierre)
+ * @version (10/11/22)
+ */
+public class sodaTester_ExtraCredit
+{
+    public static void main (String [] args)
+    {
+        SodaMachine_ExtraCredit soda = new SodaMachine_ExtraCredit();
+        soda.getTemp();
+        soda.getPrice();
+        soda.getMoney();
+        soda.getChange();
+    }
+}
